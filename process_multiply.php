202a28f9ade89391963bfed7dc7515d67b4b8439
std-faisan <150876327+std-faisan@users.noreply.github.com>
2023-11-15 11:57:10 +0700

    งานใหม่

---
+++ process_multiply.php
@@ -0,0 +1,172 @@
+<?php
+
+
+
+$pre = $_POST
+
+// สูรตคูณ แม่1
+$pre1 = 1;
+$m1 = 1;
+$n1 =12;
+
+while($pre <= $n){
+    echo $m." X ".$pre." = ".($m * $pre);
+    echo "<br>";
+    $pre++;
+}
+
+// สูรตคูณ แม่2
+$pre2 = 1;
+$m2 = 2;
+$n2 =12;
+
+while($pre <= $n){
+    echo $m." X ".$pre." = ".($m * $pre);
+    echo "<br>";
+    $pre++;
+}
+
+// สูรตคูณ แม่3
+$pre2 = 1;
+$m2 = 3;
+$n2 =12;
+
+while($pre <= $n){
+    echo $m." X ".$pre." = ".($m * $pre);
+    echo "<br>";
+    $pre++;
+}
+
+// สูรตคูณ แม่4
+$pre2 = 1;
+$m2 = 4;
+$n2 =12;
+
+while($pre <= $n){
+    echo $m." X ".$pre." = ".($m * $pre);
+    echo "<br>";
+    $pre++;
+}
+
+// สูรตคูณ แม่5
+$pre2 = 1;
+$m2 = 5;
+$n2 =12;
+
+while($pre <= $n){
+    echo $m." X ".$pre." = ".($m * $pre);
+    echo "<br>";
+    $pre++;
+}
+
+// สูรตคูณ แม่6
+$pre2 = 1;
+$m2 = 6;
+$n2 =12;
+
+while($pre <= $n){
+    echo $m." X ".$pre." = ".($m * $pre);
+    echo "<br>";
+    $pre++;
+}
+
+// สูรตคูณ แม่7
+$pre2 = 1;
+$m2 = 7;
+$n2 =12;
+
+while($pre <= $n){
+    echo $m." X ".$pre." = ".($m * $pre);
+    echo "<br>";
+    $pre++;
+}
+
+// สูรตคูณ แม่8
+$pre2 = 1;
+$m2 = 8;
+$n2 =12;
+
+while($pre <= $n){
+    echo $m." X ".$pre." = ".($m * $pre);
+    echo "<br>";
+    $pre++;
+}
+
+// สูรตคูณ แม่9
+$pre2 = 1;
+$m2 = 9;
+$n2 =12;
+
+while($pre <= $n){
+    echo $m." X ".$pre." = ".($m * $pre);
+    echo "<br>";
+    $pre++;
+}
+
+// สูรตคูณ แม่10
+$pre2 = 1;
+$m2 = 10;
+$n2 =12;
+
+while($pre <= $n){
+    echo $m." X ".$pre." = ".($m * $pre);
+    echo "<br>";
+    $pre++;
+}
+
+// สูรตคูณ แม่11
+$pre2 = 1;
+$m2 = 11;
+$n2 =12;
+
+while($pre <= $n){
+    echo $m." X ".$pre." = ".($m * $pre);
+    echo "<br>";
+    $pre++;
+}
+
+// สูรตคูณ แม่12
+$pre2 = 1;
+$m2 = 12;
+$n2 =12;
+
+while($pre <= $n){
+    echo $m." X ".$pre." = ".($m * $pre);
+    echo "<br>";
+    $pre++;
+}
+
+// สูรตคูณ แม่13
+$pre2 = 1;
+$m2 = 13;
+$n2 =12;
+
+while($pre <= $n){
+    echo $m." X ".$pre." = ".($m * $pre);
+    echo "<br>";
+    $pre++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ while.php
@@ -0,0 +1,67 @@
+<?php
+
+
+// echo $num."<br>";
+// echo ++$num."<br>";
+// echo ++$num."<br>";
+// echo ++$num."<br>";
+// echo ++$num."<br>";
+// echo ++$num."<br>";
+
+$num1 = 1;
+// เเสดงเลข 1-5
+while($num1 <= 5){
+    echo $num1."<br>";
+    $num1++;
+}
+echo "<hr color='red'>";
+
+
+$num2 = 10;
+// เเสดงเลข 10-1
+while($num2 >=1){
+    echo $num2."<br>";
+    $num2--;
+}
+echo "<hr color='red'>";
+
+$num3 = 10;
+// เเสดงเลข 10-100
+while($num3 <=100){
+    echo $num3."<br>";
+    $num3 = $num3 +5;
+    // $num3 +=5;
+}
+echo "<hr color='red'>";
+
+$num4 = 100;
+// เเสดงเลข 100-10
+while($num4 >=10){
+    echo $num4."<br>";
+    $num4 = $num4 -10;
+    // $num4 -=10;
+}
+echo "<hr color='red'>";
+
+
+// สูตรคูณเเม่99
+// จาก 1 x 12
+$pre = 1;
+$m = 99;
+$n =12;
+while($pre <= $n){
+    echo $m." X ".$pre." = ".($m * $pre);
+    echo "<br>";
+    $pre++;
+
+}
+
+
+
+
+
+
+
+
+
+?>
